10ad86a72c214b098ec0651272a9c2ce7b40da50
Jordan Ratanadawong <Jordan@Ericas-MacBook-Pro.local>
2016-04-05 20:52:13 -0700

    Updated calculator into REPL

--- Calculator.rb
+++ Calculator.rb
@@ -1,11 +1,9 @@
 class Calculator
   def add(a, b)
-   # your code here
+    puts "add"
     a + b
   end
-
   def subtract(a, b)
-   # your code here
     a - b
   end
   def multiply(a, b)
@@ -14,4 +12,20 @@ class Calculator
   def divide(a,b)
   	a / b
   end
+end
+myCalc = Calculator.new
+puts "Enter your equation using + - * or / seperating your operator and numbers with a space."
+user_input = gets.chomp
+user_input_array = user_input.split(' ')
+int1 = user_input_array[0].to_i
+int2 = user_input_array[2].to_i
+case user_input_array[1]
+  when "+"
+    puts myCalc.add(int1, int2)
+  when "-"
+    puts myCalc.subtract(int1, int2)
+  when "*"
+    puts myCalc.multiply(int1, int2)
+  when "/"
+    puts myCalc.divide(int1, int2)
 end
